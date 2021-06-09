92080cc22651e25a47fae034dfea8ac4c1bbc284
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2021-06-09 01:39:54 -0600

    Update header to become sticky

--- devfolio/src/components/NavItems/header.js
+++ devfolio/src/components/NavItems/header.js
@@ -1,29 +1,45 @@
-import * as React from "react"
+import  React,{ useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import NavItems from "./navItems"
 import * as styles from "./header.module.css"
 import NavLogo from "./NavLogo/navLogo"
 
-const Header = (props ) => (
-  
-  <header className={`${styles.headerbar} ${styles.headerpinned}`}>
-    <div className={styles.bar}>
-      <NavLogo />
-      <NavItems data={props}/>
-    </div>
-  </header>
-)
+const Header = props => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  const handleScroll = () => {     
+    const currentScrollPos = window.pageYOffset;
+    setVisible((prevScrollPos > currentScrollPos ))//&& prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
+    setPrevScrollPos(currentScrollPos);
+    //console.log("prev " + prevScrollPos +" current:"+currentScrollPos)
+  };
+
+  useEffect(() => {   
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [prevScrollPos, visible, handleScroll]);
+
+  return (
+    <header className={`${styles.headerbar} ${styles.headerpinned}`}  style={ {top: visible ? '0' : '-60px'}}>
+      <div className={styles.bar}>
+        <NavLogo />
+        <NavItems data={props} />
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-  props:PropTypes.object
+  props: PropTypes.object,
 }
 
 Header.defaultProps = {
   siteTitle: ``,
-  props: null
+  props: null,
 }
-
 
 export default Header
